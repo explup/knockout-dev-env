f43c0409e7347bb1045eec12065145aa703df695
explup <explup@gmail.com>
2017-10-11 18:41:54 +0200

    Add project files.

---
+++ knockout-dev-environment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="knockout_dev_environment.MvcApplication" Language="C#" %>
---
+++ knockout-dev-environment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(knockout_dev_environment.Startup))]
+namespace knockout_dev_environment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
